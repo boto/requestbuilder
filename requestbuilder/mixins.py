0407bb3b150451333ff8f5817cebfa6b9caf6471
Garrett Holmstrom <gholms@fedoraproject.org>
2013-04-25 14:50:50 -0700

    Add FileTransferProgressBarMixin

--- requestbuilder/mixins.py
+++ requestbuilder/mixins.py
@@ -12,9 +12,14 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
 # OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from . import Arg
-from .command import BaseCommand
+import math
+from requestbuilder import Arg, MutuallyExclusiveArgList
+import sys
 
+try:
+    import progressbar
+except ImportError:
+    pass
 
 
 class TabifyingCommand(object):
@@ -41,3 +46,105 @@ class TabifyingCommand(object):
         else:
             fstr = ''
         return '\t'.join([str(f) if allowable(f) else fstr for f in fields])
+
+
+if 'progressbar' in sys.modules:
+    _PROGRESS_BAR_COMMAND_ARGS = [
+        MutuallyExclusiveArgList(
+            Arg('--progress', dest='show_progress', action='store_true',
+                default=sys.stdout.isatty(), route_to=None,
+                help='show progress (the default when run interactively)'),
+            Arg('--no-progress', dest='show_progress', action='store_false',
+                default=sys.stdout.isatty(), route_to=None, help='''do not
+                show progress (the default when run non-interactively)'''))]
+else:
+    _PROGRESS_BAR_COMMAND_ARGS = []
+
+
+class FileTransferProgressBarMixin(object):
+    '''
+    A command mixin that provides download/upload progress bar support,
+    along with options to enable or disable them.  If progress bars are
+    disabled at the command line get_progressbar will return None.  If the
+    progressbar module is unavailable get_progressbar will return None *and*
+    no progress-related options will be added.
+    '''
+
+    ARGS = _PROGRESS_BAR_COMMAND_ARGS
+
+    def get_progressbar(self, label=None, maxval=None):
+        if 'progressbar' in sys.modules and self.args.get('show_progress',
+                                                          False):
+            widgets = []
+            if label is not None:
+                widgets += [label, ' ']
+            if maxval is not None:
+                widgets += [progressbar.Percentage(), ' ',
+                            progressbar.Bar(marker='='), ' ',
+                            _FileSize(), ' ',
+                            progressbar.FileTransferSpeed(), ' ']
+                if 'AdaptiveETA' in dir(progressbar):
+                    widgets.append(progressbar.AdaptiveETA())
+                else:
+                    widgets.append(progressbar.ETA())
+                return progressbar.ProgressBar(widgets=widgets,
+                                               maxval=(maxval or sys.maxint),
+                                               poll=0.05)
+            else:
+                widgets += [_IndeterminateBouncingBar(marker='='), ' ',
+                            _FileSize(), ' ',
+                            progressbar.FileTransferSpeed(), ' ',
+                            progressbar.Timer(format='Time: %s')]
+                return _IndeterminateProgressBar(widgets=widgets,
+                                                 maxval=(maxval or sys.maxint),
+                                                 poll=0.05)
+        else:
+            return _EveryMethodObject()
+
+
+# Used as a placeholder for ProgressBar when progressbar isn't there
+class _EveryMethodObject(object):
+    def do_nothing(self, *args, **kwargs):
+        pass
+
+    def __getattribute__(self, name):
+        return object.__getattribute__(self, 'do_nothing')
+
+
+if 'progressbar' in sys.modules:
+    class _IndeterminateProgressBar(progressbar.ProgressBar):
+        def finish(self):
+            self.maxval = self.currval
+            progressbar.ProgressBar.finish(self)
+
+
+    class _IndeterminateBouncingBar(progressbar.BouncingBar):
+        '''
+        A BouncingBar that moves exactly one space each time it updates,
+        rather than one space per unit.  This is mainly used for downloads with
+        unknown lengths.
+        '''
+        def __init__(self, *args, **kwargs):
+            progressbar.BouncingBar.__init__(self, *args, **kwargs)
+            self.__update_count = 0
+
+        def update(self, pbar, width):
+            orig_currval = pbar.currval
+            pbar.currval = self.__update_count
+            retval = progressbar.BouncingBar.update(self, pbar, width)
+            pbar.currval = orig_currval
+            self.__update_count += 1
+            return retval
+
+
+    class _FileSize(progressbar.Widget):
+        PREFIXES = ' kMGTPEZY'
+
+        def update(self, pbar):
+            if pbar.currval == 0:
+                power = 0
+                scaledval = 0
+            else:
+                power = int(math.log(pbar.currval, 1024))
+                scaledval = pbar.currval / 1024.0 ** power
+            return '{0:6.2f} {1}B'.format(scaledval, self.PREFIXES[power])
