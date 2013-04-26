9116ef655ed26a286a162fb82d8d2a800ab90254
Garrett Holmstrom <gholms@fedoraproject.org>
2013-04-26 12:43:27 -0700

    Version bump (0.1.0 alpha 9)

--- requestbuilder/__init__.py
+++ requestbuilder/__init__.py
@@ -18,7 +18,7 @@ import os.path
 import subprocess
 
 
-__version__ = '0.1.0-alpha7'
+__version__ = '0.1.0-alpha9'
 
 if '__file__' in globals():
     # Check if this is a git repo; maybe we can get more precise version info
