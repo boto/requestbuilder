f820fd20f2a4822c7742e91be785acb1fc3ff388
Garrett Holmstrom <gholms@devzero.com>
2014-07-31 13:28:06 -0700

    Version bump (0.2.3)

--- requestbuilder/__init__.py
+++ requestbuilder/__init__.py
@@ -18,7 +18,7 @@ import os.path
 import subprocess
 
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 if '__file__' in globals():
     # Check if this is a git repo; maybe we can get more precise version info
