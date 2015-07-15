008bb79d678dee91ba93af71ce04dcefda404172
Garrett Holmstrom <gholms@devzero.com>
2015-07-15 16:11:15 -0700

    Add TableOutputMixin

--- requestbuilder/mixins.py
+++ requestbuilder/mixins.py
@@ -14,6 +14,7 @@
 
 import argparse
 import math
+import operator
 import os
 import signal
 import sys
@@ -21,6 +22,10 @@ import time
 
 try:
     import progressbar
+except ImportError:
+    pass
+try:
+    import prettytable
 except ImportError:
     pass
 
@@ -115,7 +120,89 @@ class TabifyingMixin(object):
             fstr = '(nil)'
         else:
             fstr = ''
-        return '\t'.join([str(f) if allowable(f) else fstr for f in fields])
+        return '\t'.join(_filter_row_values(fields, fstr, include=include))
+
+
+class TableOutputMixin(object):
+    ARGS = [Arg('--show-headers', action='store_true', route_to=None,
+                help='show column headers'),
+            Arg('--show-empty-fields', action='store_true', route_to=None,
+                help='show empty field values as "(nil)"')]
+
+    def get_table(self, field_names):
+        table = _FilteredTable(field_names=field_names,
+                               show_empty=self.args.get('show_empty_fields'))
+        table.border = False
+        table.header = self.args.get('show_headers') or False
+        table.header_style = 'upper'
+        table.align = 'l'  # left
+        table.left_padding_width = 0
+        table.right_padding_width = 2
+        return table
+
+
+if 'prettytable' in sys.modules:
+    class _FilteredTable(prettytable.PrettyTable):
+        def __init__(self, show_empty=False, **kwargs):
+            if show_empty:
+                self.__empty = '(nil)'
+            else:
+                self.__empty = ''
+            prettytable.PrettyTable.__init__(self, **kwargs)
+
+        def add_row(self, row):
+            prettytable.PrettyTable.add_row(
+                self, _filter_row_values(row, self.__empty))
+else:
+    # UglyTable
+    class _FilteredTable(object):
+        def __init__(self, field_names, show_empty=False):
+            self.field_names = field_names
+            self.header = False
+            self.reversesort = False
+            self._rows = []
+            self._sortindex = 0
+            if show_empty:
+                self.__empty = '(nil)'
+            else:
+                self.__empty = ''
+
+        def add_row(self, row):
+            if len(row) != len(self.field_names):
+                raise ValueError('row has incorrect number of values '
+                                '({0} given, {1} expected)'
+                                .format(len(row), len(field_names)))
+            self._rows.append(_filter_row_values(row, self.__empty))
+
+        @property
+        def sortby(self):
+            return self.field_names[self._sortindex]
+
+        @sortby.setter
+        def sortby(self, field):
+            self._sortindex = self.field_names.index(field)
+
+        def get_string(self):
+            lines = []
+            if self.header:
+                lines.append('\t'.join(name.upper() for name in self.field_names))
+            for row in sorted(self._rows, reverse=self.reversesort,
+                              key=operator.itemgetter(self._sortindex)):
+                lines.append('\t'.join(map(str, row)))
+            return '\n'.join(lines)
+
+        def __str__(self):
+            return self.get_string()
+
+
+def _filter_row_values(row, empty_str, include=None):
+    filtered = []
+    for field in row:
+        if field or field is 0 or field in (include or []):
+            filtered.append(field)
+        else:
+            filtered.append(empty_str)
+    return filtered
 
 
 if 'progressbar' in sys.modules:
